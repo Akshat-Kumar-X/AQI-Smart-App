96f4623ea1532325e2c9d9e746015c97c3a09e15
Ebrahim Mamawala <ebrahimmmamawala@gmail.com>
2025-01-14 23:07:00 +0530

    Add German AQI Calculation

--- api/index.js
+++ api/index.js
@@ -28,7 +28,8 @@ const smartApp = new SmartApp()
                 .options([
                     { id: 'cai', name: 'CAI (Comprehensive Air Quality Index)' },
                     { id: 'epa', name: 'EPA (US Environmental Protection Agency)' },
-                    { id: 'naqi', name: 'NAQI (National Air Quality Index)' }
+                    { id: 'naqi', name: 'NAQI (National Air Quality Index)' },
+                    { id: 'fea', name: 'FEA (Germany Federal Environmental Agency)'}
                 ])
                 .required(true)
                 .name('Standard Type');
--- library/index.js
+++ library/index.js
@@ -1,12 +1,14 @@
 const CAI_Calculator = require('./AQI-Calculators/cai');
 const EPA_Calculator = require('./AQI-Calculators/epa');
 const NAQI_Calculator = require('./AQI-Calculators/naqi');
+const FEA_Calculator = require('./AQI-Calculators/fea');
 
 // Add more calculators as you expand to more standards
 const calculators = {
     cai: CAI_Calculator,
     epa: EPA_Calculator,
     naqi: NAQI_Calculator,
+    fea: FEA_Calculator
 };
 
 module.exports = (standardType = 'cai') => {
